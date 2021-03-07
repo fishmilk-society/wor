4b2b91c46c6656c2f89285d76610a3bc441da444
Shane S. Anderson <1176969+ander-nz@users.noreply.github.com>
2021-03-07 13:37:50 +1300

    Added build script for TS

--- gulpfile.esm.js
+++ gulpfile.esm.js
@@ -1,20 +1,37 @@
-import gulp from "gulp";
-import sass from "gulp-dart-sass";
-import PluginError from "plugin-error";
-import gulplog from "gulplog";
+import gulp from 'gulp'
+import gulplog from 'gulplog'
+import PluginError from 'plugin-error'
+import rollup from 'gulp-rollup'
+import sass from 'gulp-dart-sass'
+import ts from 'gulp-typescript'
 
-export function css() {
-    return gulp.src("wor.sass")
+export function css()
+{
+    return gulp.src('wor.sass')
         .pipe(sass())
-        .on("error", handleError)
-        .pipe(gulp.dest("dist"));
+        .on('error', handleError)
+        .pipe(gulp.dest('dist'))
 
-    function handleError(error) {
-        const message = new PluginError("sass", error.messageOriginal).toString();
-        gulplog.error(message);
-        process.exitCode = 1;
-        this.emit("end");
+    function handleError(error)
+    {
+        const message = new PluginError('sass', error.messageOriginal).toString()
+        gulplog.error(message)
+        process.exitCode = 1
+        this.emit('end')
     }
 }
 
-export default gulp.series(css);
+export function js()
+{
+    const tsProject = ts.createProject('tsconfig.json')
+
+    return tsProject.src()
+        .pipe(tsProject())
+        .pipe(rollup({
+            input: 'wor.js',
+            output: { format: 'esm' }
+        }))
+        .pipe(gulp.dest('dist'))
+}
+
+export default gulp.series(css, js)
---
+++ wor.ts
@@ -0,0 +1,17 @@
+import { CharacterSheet } from './templates/character-sheet.js'
+
+declare const Actors: any
+declare const ActorSheet: any
+declare const Hooks: any
+
+Hooks.once('init', function()
+{
+    Actors.unregisterSheet('core', ActorSheet)
+    Actors.registerSheet('wor', CharacterSheet, {
+        label: 'Character Sheet',
+        types: ['character'],
+        makeDefault: true
+    })
+
+    console.log('WOR | Initialized')
+})
