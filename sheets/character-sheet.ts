445fe02b485d2e8db897e948f2dd2db7bf45c8d9
Shane S. Anderson <1176969+ander-nz@users.noreply.github.com>
2021-03-21 09:17:05 +1300

    Revert unrelated changes

--- sheets/character-sheet.ts
+++ sheets/character-sheet.ts
@@ -2,60 +2,12 @@ export class CharacterSheet extends ActorSheet
 {
     static get defaultOptions(): BaseEntitySheet.Options
     {
-        const template = 'systems/wor/sheets/character-sheet.hbs'
-
-        delete _templateCache[template]
-
         return {
             ...super.defaultOptions,
-            template: template,
+            template: 'systems/wor/sheets/character-sheet.hbs',
             width: 400,
             height: 'auto',
             resizable: false
-        }
-    }
-
-    activateListeners(html: JQuery)
-    {
-        super.activateListeners(html)
-
-        html.on('click', '[data-action^=wor-]', evt =>
-        {
-            this.handleAction(evt.currentTarget.dataset)
-        })
-    }
-
-    private handleAction(dataset: DOMStringMap)
-    {
-        const actor = this.actor
-
-        switch (dataset.action) {
-            case 'wor-add-effect':
-                return ActiveEffect.create({
-                    label: "New effect",
-                    icon: "icons/svg/aura.svg"
-                }, actor).create()
-
-            case 'wor-edit-effect':
-                return effect().sheet.render(true)
-
-            case 'wor-delete-effect':
-                return effect().delete()
-
-            default:
-                throw `Unknown action ‘${dataset.action}’`
-        }
-
-        function effect()
-        {
-            if (!dataset.id)
-                throw 'Missing ‘data-id’ attribute'
-
-            const effect = actor.effects.get(dataset.id)
-            if (!effect)
-                throw `Could not find effect ‘${dataset.id}’`
-
-            return effect
         }
     }
 }
