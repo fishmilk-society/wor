4e69206cdeb1baa729791422e7554c7ea84b4899
Shane S. Anderson <1176969+ander-nz@users.noreply.github.com>
2021-03-21 10:31:51 +1300

    Added active effects to character sheet (#2)
    This pull request adds a new section to the character sheet, allowing you to create and edit active effects.

--- sheets/character-sheet.ts
+++ sheets/character-sheet.ts
@@ -10,4 +10,48 @@ export class CharacterSheet extends ActorSheet
             resizable: false
         }
     }
+
+    activateListeners(html: JQuery)
+    {
+        super.activateListeners(html)
+
+        html.on('click', '[data-action^=wor-]', evt =>
+        {
+            this.handleAction(evt.currentTarget.dataset)
+        })
+    }
+
+    private handleAction(dataset: DOMStringMap)
+    {
+        const actor = this.actor
+
+        switch (dataset.action) {
+            case 'wor-add-effect':
+                return ActiveEffect.create({
+                    label: "New effect",
+                    icon: "icons/svg/aura.svg"
+                }, actor).create()
+
+            case 'wor-edit-effect':
+                return effect().sheet.render(true)
+
+            case 'wor-delete-effect':
+                return effect().delete()
+
+            default:
+                throw `Unknown action ‘${dataset.action}’`
+        }
+
+        function effect()
+        {
+            if (!dataset.id)
+                throw 'Missing ‘data-id’ attribute'
+
+            const effect = actor.effects.get(dataset.id)
+            if (!effect)
+                throw `Could not find effect ‘${dataset.id}’`
+
+            return effect
+        }
+    }
 }
