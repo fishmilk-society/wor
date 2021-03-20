f8d03b6c513dbf655a46868650361e8a6651bc28
Shane S. Anderson <1176969+ander-nz@users.noreply.github.com>
2021-03-21 09:22:56 +1300

    Refactor CSS (#3)
    This pull request makes the following changes:
    * Prefixes all CSS classes.
    * Normalises all CSS classes to not depend on Foundry’s built-in styles.
    * Moves the textual content of section headers into a `<span>`.
    
    The above refactoring does not apply to `.form-group` which is a built-in CSS class.

--- sheets/character-sheet.ts
+++ sheets/character-sheet.ts
@@ -4,7 +4,6 @@ export class CharacterSheet extends ActorSheet
     {
         return {
             ...super.defaultOptions,
-            classes: ['wor', 'character-sheet'],
             template: 'systems/wor/sheets/character-sheet.hbs',
             width: 400,
             height: 'auto',
