a8476878ada0670ec119e51e6487796502e48a27
Shane S. Anderson <1176969+ander-nz@users.noreply.github.com>
2021-05-29 15:39:32 +1200

    Automatically disable when expired

--- src/entities/effect.ts
+++ src/entities/effect.ts
@@ -2,6 +2,38 @@ import Duration from "../helpers/duration"
 
 export class Effect extends ActiveEffect
 {
+    get expired(): boolean
+    {
+        return !!this.getFlag('wor', 'expired')
+    }
+
+    get shouldBeExpired(): boolean
+    {
+        let d = this.data.duration
+
+        if (d.startTime && d.seconds)
+        {
+            const remaining = d.startTime + d.seconds - game.time.worldTime
+
+            if (remaining < 0)
+                return true
+        }
+
+        return false
+    }
+
+    get isTemporary(): boolean
+    {
+        return super.isTemporary && !this.expired
+    }
+
+    apply(actor: Actor, change: ActiveEffectChange)
+    {
+        if (this.expired)
+            return
+        super.apply(actor, change)
+    }
+
     get remaining(): string
     {
         let d = this.data.duration
@@ -21,3 +53,25 @@ export class Effect extends ActiveEffect
         return 'unknown'
     }
 }
+
+Hooks.on('updateWorldTime', (worldTime, dt) =>
+{
+    game.actors?.forEach(actor =>
+    {
+        let updates
+
+        actor.effects.forEach(effectObj =>
+        {
+            const effect = effectObj as Effect
+            const shouldBe = effect.shouldBeExpired
+            if (effect.expired != shouldBe)
+            {
+                updates ??= []
+                updates.push({ _id: effect.id, 'flags.wor.expired': shouldBe })
+            }
+        })
+
+        if (updates)
+            actor.updateEmbeddedEntity('ActiveEffect', updates)
+    })
+})
