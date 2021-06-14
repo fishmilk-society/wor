857670d6921e7e7ecd6cc83e86e6344b0852a85f
Shane S. Anderson <1176969+ander-nz@users.noreply.github.com>
2021-06-14 18:06:40 +1200

    docos and cleanup

--- src/allow-negative-hp.ts
+++ src/allow-negative-hp.ts
@@ -1,25 +1,60 @@
-function replaceOnce(fn: string, FIND: string, REPLACE: string): string
+/**
+ * @file This module changes the token HUD to allow negative numbers in the HP field.
+ */
+
+/**
+ * Mix functionality into the @see Actor class.
+ */
+Hooks.once('init', function()
 {
-    const index = fn.indexOf(FIND)
+    patch(Actor, 'modifyTokenAttribute', fn =>
+    {
+        // Since the original was defined in a class but this one is just a loose function:
+        fn = replaceOnce(fn, `async modifyTokenAttribute`, `async function modifyTokenAttribute`)
+
+        // Remove the lower bound:
+        fn = replaceOnce(fn, 'Math.clamped(0, ', 'Math.min(')
+
+        return fn
+    })
+})
+
+/**
+ * Replaces a string exactly one time. If @param find is not found, or is found more than once,
+ * this method will throw an error.
+ * @param contents The string to modify.
+ * @param find What to search for.
+ * @param replace What to replace it with.
+ * @returns The modified string.
+ */
+function replaceOnce(contents: string, find: string, replace: string): string
+{
+    const index = contents.indexOf(find)
 
     if (index == -1)
-        throw new Error(`Could not find string ‘${FIND}’`)
+        throw new Error(`Could not find string ‘${find}’`)
 
-    if (fn.indexOf(FIND, index + 1) != -1)
-        throw new Error(`Found string ‘${FIND}’ more than once`)
+    if (contents.indexOf(find, index + 1) != -1)
+        throw new Error(`Found string ‘${find}’ more than once`)
 
-    return fn.substring(0, index) + REPLACE + fn.substring(index + FIND.length)
+    return contents.substring(0, index) + replace + contents.substring(index + find.length)
 }
 
-type ClassOf<T> = {
-    new(...args: any): T
-    prototype: T
-}
-
+/**
+ * Modifies an instance method using string manipulation.
+ * @param type The class that contains the method.
+ * @param key The name of the method to replace.
+ * @param patcher A function which, given a function definition as a string, returns the new
+ * function definition.
+ */
 function patch<
     T extends { [key in K]: Function },
     K extends keyof T
->(type: ClassOf<T>, key: K, patcher: (fn: string) => string)
+>(
+    type: { new(...args: any): T; prototype: T },
+    key: K,
+    patcher: (fn: string) => string
+)
 {
     try
     {
@@ -32,13 +67,3 @@ function patch<
         throw new Error(`Error patching ‘${type.name}.${key}’: ${error.message}`)
     }
 }
-
-Hooks.once('init', function()
-{
-    patch(Actor, 'modifyTokenAttribute', fn =>
-    {
-        fn = replaceOnce(fn, `async modifyTokenAttribute`, `async function modifyTokenAttribute`)
-        fn = replaceOnce(fn, 'Math.clamped(0, ', 'Math.min(')
-        return fn
-    })
-})
