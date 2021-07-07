b76508652ef0607374bc5ad406aca8db25b5e7c4
Shane S. Anderson <1176969+ander-nz@users.noreply.github.com>
2021-07-07 12:57:27 +1200

    X/Y offset sliders for tokens (#22)

---
+++ src/tokens/image-anchor.ts
@@ -0,0 +1,77 @@
+/**
+ * @file
+ * This module adds ‘x/y offset’ sliders to the token configuration dialog. These sliders can be
+ * used to modify the position of the token image relative to its base.
+ */
+
+import { requireElement } from "../helpers/require-element"
+
+/**
+ * The relevant flags for this module.
+ */
+type TokenFlags = {
+    wor?: {
+        anchor?: { x: number; y: number }
+    }
+}
+
+/**
+ * This method is like `getFlag` but faster. That’s important since this method may be called quite
+ * often.
+ */
+function getAnchorFast(token: Token): { x: number; y: number } | undefined
+{
+    const flags = token.data.flags as TokenFlags
+    return flags.wor?.anchor
+}
+
+/**
+ * Add UI to the token configuration dialog.
+ */
+Hooks.on<Hooks.RenderApplication<object, TokenConfig>>('renderTokenConfig', function({ token }, html)
+{
+    // Find the ‘scale’ slider, since we’ll be adding the offset fields underneath that:
+    const scaleSlider = requireElement(html, 'scale', HTMLInputElement)
+
+    // Get the current value of the offset fields:
+    const anchor = getAnchorFast(token) ?? { x: 0.5, y: 0.5 }
+
+    // Add the offset fields:
+    $(scaleSlider).closest('.form-group').after(`
+         <div class='form-group'>
+             <label>X Offset <span class='units'>(Ratio)</span>:</label>
+             <div class='form-fields'>
+                 <input type='range' name='flags.wor.anchor.x' value='${anchor.x}' min='0' max='1' step='0.01' data-dtype='Number'>
+                 <span class='range-value'>${anchor.x}</span>
+             </div>
+         </div>
+         <div class='form-group'>
+             <label>Y Offset <span class='units'>(Ratio)</span>:</label>
+             <div class='form-fields'>
+                 <input type='range' name='flags.wor.anchor.y' value='${anchor.y}' min='0' max='1' step='0.01' data-dtype='Number'>
+                 <span class='range-value'>${anchor.y}</span>
+             </div>
+         </div>`)
+})
+
+/**
+ * Add the ‘offset’ functionality into the {@link Token} class.
+ */
+Token.prototype.refresh = (function()
+{
+    const original = Token.prototype.refresh
+
+    return function(this: Token)
+    {
+        original.apply(this)
+
+        const anchor = getAnchorFast(this)
+        if (anchor)
+        {
+            this.icon!.position.set(
+                this.w * anchor.x,
+                this.h * anchor.y
+            )
+        }
+    }
+})()
