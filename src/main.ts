d65eda1818a28d287b7a9c14dbcf1383cf9dc364
Shane S. Anderson <1176969+ander-nz@users.noreply.github.com>
2021-10-02 22:06:19 +1300

    Tidy-up module names (#43)

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,16 @@
 import './allow-negative-hp'
 import './active-effects'
-import './tokens/high-precision-scale'
-import './tokens/image-anchor'
-import './tokens/preview-changes'
 import './initiative'
-import './update-slider-values'
 import { watch } from './live-reload.js'
 import { CharacterSheet } from './sheets/character-sheet.js'
 
 import './foundrymq/mod:add-show-logs-button'
 import './foundrymq/mod:handle-update-actor-messages'
+import './general/mod:more-interactive-fields'
 import './migrations/mod:run-migrations'
+import './tokens/mod:high-precision-scale'
+import './tokens/mod:image-anchor'
+import './tokens/mod:preview-changes'
 import './tokens/mod:use-actor-size'
 
 Hooks.once('init', function()
