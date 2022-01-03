5a54c7971f8f97a47908b90d6cd5b0062c0a0872
Shane S. Anderson <1176969+ander-nz@users.noreply.github.com>
2022-01-03 15:43:37 +1300

    Position status effect dialog near character sheet

--- src/characters/CharacterSheet/index.ts
+++ src/characters/CharacterSheet/index.ts
@@ -143,7 +143,10 @@ export class CharacterSheet extends ActorSheet
 
         function showEditorFor(effect: StatusEffect)
         {
-            effect.sheet.render(true)
+            effect.sheet.render(true, {
+                top: unwrap(sheet.position.top) + 40,
+                left: unwrap(sheet.position.left) + 10
+            })
         }
     }
 
